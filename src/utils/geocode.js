16da2b3a75660425b5e1e1de95f9de4101fba27f
Mitchell Fontaine <mitchellf@UHLCO.local>
2019-04-22 13:58:27 -0500

    Initial commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?limit=1&access_token=pk.eyJ1IjoibWlmMjI3MzQiLCJhIjoiY2p1OGp5aWNmMjYyODQ0cnYzcnd1aDdueSJ9.627Ku_achGuJSJCmnatXwA'
+
+    request({url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!')     
+        } else if (body.features.length === 0) {
+             callback('Unable to find coordinates for location!')
+        } else {
+            callback(undefined,  {
+               latitude: body.features[0].center[1],
+               longitude: body.features[0].center[0],
+               location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
