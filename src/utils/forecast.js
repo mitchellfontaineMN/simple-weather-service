c20ae7cf7c7008dd7c14f39c33b3fc9dedff566e
Mitchell Fontaine <mitchellf@UHLCO.local>
2019-04-22 15:12:42 -0500

    Added daily high and low to forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!');
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The high for today is ' + body.daily.data[0].temperatureHigh + ' and the low will be ' + body.daily.data[0].temperatureLow + '. There is a ' + body.daily.data[0].precipProbability*100 + '% chance of rain.')
         }
     });
 }
